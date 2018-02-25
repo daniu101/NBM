b3b6446aa5f5691c454bb5ad0876dc21cb431585
Administrator <Administrator@PC-20170518KDMT>
2018-02-25 15:42:11 +0800

    更新

--- NBM/console_model.py
+++ NBM/console_model.py
@@ -5,8 +5,6 @@ from NBM.pre_data import *
 from NBM.conditional_probability import *
 from NBM.probability_classification import *
 
-FILE_PATH =  "D:/5/"
-
 # 加载每句话 和属性标签 label
 listOPosts,listClasses = loadDataSet()
 # 形成字典
@@ -19,9 +17,10 @@ for postinDoc in listOPosts:
 
 # 把转好的所有语句特征List 和 属性标签Label 进行条件概率计算
 p0V,p1V,pAb = trainNB0(array(trainMat),array(listClasses))
-print(p0V)
-print(p1V)
-print(pAb)
+print("字典：",myVocabList)
+print("p0V:",p0V)
+print("p1V:",p1V)
+print("pAb:",pAb)
 # 需要判断的语句
 testEntry = ['大量', '代开']
 thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
